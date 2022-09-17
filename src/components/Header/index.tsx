c47def6d89b53e3de7943a917416e641c48d52e8
Chris Lovejoy <chrislovejoy@chriss-mbp.home>
2022-09-17 21:19:24 +0100

    add button to toggle masti and ustaad, display mode in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { IoInformationCircleOutline } from "react-icons/io5";
+import { BsCircle } from "react-icons/bs";
 import { isPropertySignature } from "typescript";
 
 import * as Styled from "./index.styled";
@@ -14,24 +15,33 @@ interface Props {
 export function Header(headerProps: Props) {
   return (
     <Styled.Container>
-      <Styled.Content>
+      <Styled.ToggleButtons>
         <IoInformationCircleOutline
           onClick={headerProps.openInfoPopUp}
           size={30}
           width={30}
           height={30}
         />
-
         {headerProps.gameMode === "Masti" && (
-          <button onClick={headerProps.toUstaadGameMode}>to ustaad</button>
+          <BsCircle
+            onClick={headerProps.toUstaadGameMode}
+            size={30}
+            width={30}
+            height={30}
+          />
         )}
         {headerProps.gameMode === "Ustaad" && (
-          <button onClick={headerProps.toMastiGameMode}>to masti</button>
+          <BsCircle
+            onClick={headerProps.toMastiGameMode}
+            size={30}
+            width={30}
+            height={30}
+          />
         )}
-
+      </Styled.ToggleButtons>
+      <Styled.Content>
         <Styled.Logo>Geetle</Styled.Logo>
-        {headerProps.gameMode}
-        <a href="#"></a>
+        <p>{headerProps.gameMode}</p>
       </Styled.Content>
     </Styled.Container>
   );
