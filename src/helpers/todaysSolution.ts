8eb147199f4dcb028dd09af339f62d629958af0b
sobti619 <114086037+sobti619@users.noreply.github.com>
2022-09-29 20:08:06 +0100

    Update todaysSolution.ts

--- src/helpers/todaysSolution.ts
+++ src/helpers/todaysSolution.ts
@@ -1,6 +1,6 @@
 import { songs } from "../constants";
 
-const epochMs = new Date(2022, 2, 21).valueOf();
+const epochMs = new Date(2022, 9, 28).valueOf();
 const now = Date.now();
 const msInDay = 86400000;
 const index = Math.floor((now - epochMs) / msInDay);
