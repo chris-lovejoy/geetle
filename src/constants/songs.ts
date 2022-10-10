67735ba7d823fd2a5967151479252e9675f82722
sobti619 <114086037+sobti619@users.noreply.github.com>
2022-10-11 00:16:43 +0100

    Update songs.ts

--- src/constants/songs.ts
+++ src/constants/songs.ts
@@ -69,7 +69,7 @@ export const songs = [
     movie: "Namak Halaal",
     name: "Ke Pag Ghunghroo Bandh Meera",
     youtubeId: "SzxeWJjjVd4",
-    startTime: 18,
+    startTime: 19,
   },
   {
     movie: "Band Baaja Baaraat",
