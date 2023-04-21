22d027099370070e3a13fbc56c0604c9d4985a6e
sobti619 <114086037+sobti619@users.noreply.github.com>
2023-04-21 10:43:38 +0100

    Update todaysSolution.ts
    changed start date to continue list

--- src/helpers/todaysSolution.ts
+++ src/helpers/todaysSolution.ts
@@ -1,6 +1,6 @@
 import { songs } from "../constants";
 
-const epochMs = new Date(2022, 8, 30).valueOf();
+const epochMs = new Date(2023, 3, 01).valueOf();
 const now = Date.now();
 const msInDay = 86400000;
 const index = Math.floor((now - epochMs) / msInDay);
